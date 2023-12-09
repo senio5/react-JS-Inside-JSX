2e324ddf4bf00573bedaad645574629b493ee3b3
senio5 <arsenio.taylor5@gmail.com>
2023-12-09 15:53:54 -0500

    react JS inside JSX

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*its best practice to make variables and put those variables in curly brackets 
+ inside the jsx */
+function App(){
+  const date = new Date()
+  const hours = date.getHours() % 12
+  let timeOfDay
+
+  if(hours < 12){
+    timeOfDay = "morning"
+  }else if(hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  }else {
+    timeOfDay = "night"
+  }
+
+  return(
+    <h1>Good {timeOfDay}!</h1>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
